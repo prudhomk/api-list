0eacfec55181f9dcf1951f148df9c6fff818e413
Kalan Prudhomme <cleaver9@gmail.com>
2021-07-27 16:57:01 -0700

    added router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VillagerCharacters from '../../containers/VillagerCharacters';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-export default function App() {
-  return <VillagerCharacters />;
+export default class App extends Component {
+  
+  render() {
+    return (
+   
+      <Router>
+         
+
+        <Switch>
+          <Route path="/" exact={true}
+            render={routerProps => (
+              <VillagerCharacters {...routerProps}/>
+            )}
+          />
+
+        </Switch>
+          
+    
+      </Router>
+
+    );
+  }
 }
+
