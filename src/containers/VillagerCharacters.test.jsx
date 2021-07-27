d821371d0057f3410486fb2a38db35c917e346dd
Kalan Prudhomme <cleaver9@gmail.com>
2021-07-27 15:47:49 -0700

    created villagerCharacter component logic and test

---
+++ src/containers/VillagerCharacters.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import VillagerCharacters from './VillagerCharacters';
+
+describe('VillagerCharacter container', () => {
+    it('displays a list of characters', async () => {
+        render(<VillagerCharacters />);
+
+        screen.getByText('Loading...');
+
+        const ul = await screen.findByRole('list');
+        expect(ul).not.toBeEmptyDOMElement();
+    });
+});
